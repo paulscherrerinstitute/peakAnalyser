869a51cf629b4c9c5eec2e041bc573c694f65b95
Xiaoqiang Wang <xiaoqiangwang@gmail.com>
2021-09-02 14:10:37 +0200

    remove obsolete argument

--- psiBuild/startup.cmd
+++ psiBuild/startup.cmd
@@ -1,6 +1,6 @@
 set_requestfile_path("$($(MODULE)_DIR)db")
 
-peakAnalyserConfig("$(PORT=PA$(N=1))","$(PEAK_ADDRESS=http://127.0.0.1:8080)", "$(LOCAL_PORT=52320)")
+peakAnalyserConfig("$(PORT=PA$(N=1))","$(PEAK_ADDRESS=http://127.0.0.1:8080)")
 dbLoadRecords("peakAnalyser.template","P=$(PREFIX),R=$(RECORD=cam1:),PORT=$(PORT=PA$(N=1)),ADDR=$(ADDR=0),TIMEOUT=1")
 
 set_pass0_restoreFile("peakAnalyser_settings$(N=1).sav")
